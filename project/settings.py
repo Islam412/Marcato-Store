057af36262033dffda62dfe00b5ffd800f41e838
Islam412 <islam.124.hamdy@gmail.com>
2024-11-10 01:26:55 +0200

    call all apps in setting file

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'userauths',
+    'products',
+    'orders',
+    'settings',
 ]
 
 MIDDLEWARE = [
