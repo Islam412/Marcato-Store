48b7a9f48f535bf192453082317ec719ab223aa4
Islam412 <islam.124.hamdy@gmail.com>
2024-11-10 02:55:06 +0200

    create queryset_debug functions

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView , DetailView
+from django.db.models import Q , F , Value
+from django.db.models.aggregates import Max,Min,Count,Avg,Sum
 
-# Create your views here.
+
+from .models import Product, Brand, ProductImage, Review
+
+
+
+
+def queryset_debug(request):
+    
+    # data = Product.objects.all()
+    
+    # data = Product.objects.filter(price__gt=80, quantity__lt=10)  #and
+    
+    # data = Product.objects.filter(Q(price__gt=80) & Q(quantity__lt=10))  #or
+    
+    data = Product.objects.annotate(price_with_tax=F('price')*1.2) # add new tower
+    
+    return render(request, 'products/queryset_debug.html', {'data': data})
