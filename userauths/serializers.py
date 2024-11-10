05b5ad653e0e6bbfb661b0f0cf203891e0785a87
Islam412 <islam.124.hamdy@gmail.com>
2024-11-10 02:01:40 +0200

    creat ProfileSerializer p1

--- userauths/serializers.py
+++ userauths/serializers.py
@@ -9,3 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
          
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+    
