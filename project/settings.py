f811fbf50b46aec02cc251f5c4880926745363e2
Islam412 <islam.124.hamdy@gmail.com>
2024-11-10 03:08:55 +0200

    call get_company_data contexet processor

--- project/settings.py
+++ project/settings.py
@@ -93,7 +93,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
 
                 # Custom context processors off company data from settings app(company_contexet_processor.py)
-                # 'settings.company_contexet_processor.get_company_data',
+                'settings.company_contexet_processor.get_company_data',
             ],
         },
     },
