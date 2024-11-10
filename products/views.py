d6cc9e50724fd5d950b66d6547f439a29fc86d81
Islam412 <islam.124.hamdy@gmail.com>
2024-11-10 02:56:26 +0200

    create ProductDetails views

--- products/views.py
+++ products/views.py
@@ -26,3 +26,16 @@ class ProductList(ListView):
     model = Product
     paginate_by = 30
     
+
+
+class ProductDetails(DetailView):
+    model = Product
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        context["reviews"] = Review.objects.filter(product=product)
+        context["average_rating"] = product.average_rating
+        context["rate_products"] = Product.objects.filter(brand=product.brand)
+        return context
+    
