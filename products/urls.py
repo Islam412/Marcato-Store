8e9f9e0ccdc3eef6a4f6cd16c996ab29be65bac8
Islam Hamdy <islam.124.hamdy@gmail.com>
2024-12-08 11:19:14 +0200

    filter by tag html

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductDetails, ProductList, BrandList, BrandDetails, queryset_debug , product_search , product_filter
+from .views import ProductDetails, ProductList, BrandList, BrandDetails, queryset_debug , product_search , product_filter , product_filter_by_tags
 
 app_name = 'products'
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('brands/', BrandList.as_view(), name='brand_list'),
     path('search/', product_search, name='product_search'),
     path('filter/', product_filter, name='product_filter'),
+    path('filter-by-tags/', product_filter_by_tags, name='product_filter_by_tags'),
     path('', ProductList.as_view(), name='product_list'),
     path('debug/', queryset_debug, name='queryset_debug'),  # Debugging tool for testing queryset performance.
     path('<slug:slug>/', ProductDetails.as_view(), name='product_detail'),
