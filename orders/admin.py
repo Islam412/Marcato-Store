02a83dce0819bf787c0803a8f94dd54095e129db
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-01-20 18:08:44 +0200

    call models at orders in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cart,CartDetails,Order,OrderDetails,Coupon
 
 # Register your models here.
+
+admin.site.register(Cart)
+admin.site.register(CartDetails)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
+admin.site.register(Coupon)
