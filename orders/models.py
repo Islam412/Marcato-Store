5e092dd1c131ff7ffa63834864c41f90072ce3dd
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-01-20 15:30:20 +0200

    desing order details models p1

--- orders/models.py
+++ orders/models.py
@@ -16,7 +16,18 @@ class Cart(models.Model):
 
 
 class CartDetails(models.Model):
-    cart = models.ForeignKey(Cart,related_name='cart_details', on_delete=models.SET_NULL,null=True, blank=True)
+    cart = models.ForeignKey(Cart,related_name='cart_details', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='cart_product', on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField()
+    total = models.FloatField(null=True,blank=True)
+
+
+class Order(models.Model):
+    pass
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order,related_name='order_details', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_product', on_delete=models.SET_NULL, null=True, blank=True)
+    price = models.FloatField()
     quantity = models.IntegerField()
     total = models.FloatField(null=True,blank=True)
