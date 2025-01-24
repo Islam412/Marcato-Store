3893ff29b3e37bbdeac55722690050483fc86745
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-01-24 22:41:16 +0200

    add login required for functions and class based views

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required # login required for functions
+from django.contrib.auth.mixins import LoginRequiredMixin # login required for class based views
+
 
 from .models import Order , CartDetails , Cart
 
 # Create your views here.
 
-class OrderListView(ListView):
+class OrderListView(LoginRequiredMixin , ListView):
     model = Order
     ordering = ['-id']
     paginate_by = 10
@@ -15,6 +18,7 @@ class OrderListView(ListView):
         return queryset
 
 
+@login_required 
 def checkout(request):
     cart = Cart.objects.get(user=request.user,status='InProgress')
     cart_detail = CartDetails.objects.filter(cart=cart)
