5e5c013b5d165474876d6030985acd5b2c1589fd
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-01-29 16:25:02 +0200

    desing add to cart views p1

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import OrderListView, checkout
+from .views import OrderListView, checkout ,add_to_cart
 
 
 
@@ -9,4 +9,5 @@ app_name = 'orders'
 urlpatterns = [
     path('', OrderListView.as_view(), name='order_list'),
     path('checkout/', checkout, name='checkout'),
+    path('add-to-cart/', add_to_cart, name='add_to_cart'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required # login required for functions
 from django.contrib.auth.mixins import LoginRequiredMixin # login required for class based views
 
 
 from .models import Order , CartDetails , Cart
+from products.models import Product
 
 # Create your views here.
 
@@ -16,6 +17,20 @@ class OrderListView(LoginRequiredMixin , ListView):
     def get_queryset(self):
         queryset = super().get_queryset().filter(user=self.request.user)
         return queryset
+    
+
+def add_to_cart(request):
+    quantity = request.POST['quantity']
+    product = Product.objects.get(id=request.POST['product_id'])
+
+    cart = Cart.objects.get(user = request.user, status='InProgress')
+    cart_detail , creeate = CartDetails.objects.get_or_create(cart=cart, product=product)
+
+    cart_detail.quantity = int(quantity)
+    cart_detail.total = round(int(quantity)* product.price , 2)
+    cart_detail.save()
+
+    return redirect(f'/products/{product.slug}')
 
 
 @login_required 
