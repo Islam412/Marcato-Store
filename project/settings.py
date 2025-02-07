08a5b4ab42621ce00e58009981486e31e6340912
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-07 21:19:18 +0200

    add selery

--- project/settings.py
+++ project/settings.py
@@ -271,8 +271,9 @@ CACHES = {
 
 
 # django celery settings
-CELERY_URL ='redis://localhost:6379/0'
-CELERY_RESULT_BACKEND ='redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
 
 
 # JAZZMIN settings
