e80f4f16e16f1c6cdb74b6406bb99f92f4d6e02a
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-09 00:59:43 +0200

    fix erorr

--- project/settings.py
+++ project/settings.py
@@ -139,7 +139,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "MARCATO_DBpip freeze > requirements.txt",
+        "NAME": "MARCATO_DB",
         "USER": "postgres",
         "PASSWORD": "1111",
         "HOST": "127.0.0.1",
