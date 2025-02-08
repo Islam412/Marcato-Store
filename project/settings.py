83dfa7c654c5457a59462d7859bd2f7c242f03ff
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-02-08 21:52:34 +0200

    django connection with postgresql database

--- project/settings.py
+++ project/settings.py
@@ -128,10 +128,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# PostgreSQL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "MARCATO_DB",
+        "USER": "postgres",
+        "PASSWORD": "1111",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
